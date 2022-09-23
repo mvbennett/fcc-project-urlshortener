08d62030c3050f5bd1e030238565a9d5017b5436
Mike Bennett <mvbennett9@gmail.com>
2022-09-23 18:11:49 +0900

    post request gets parsed

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+// const multer = require('multer');
+// const upload = multer();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,9 +17,17 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', (req, res, next) => {
+  res.json({original_url: req.body['url']});
+  next();
 });
 
 app.listen(port, function() {
