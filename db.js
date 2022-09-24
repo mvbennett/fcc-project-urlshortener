736cf524aad6181ef6a450f6620a09c15f9e0db7
Mike Bennett <mvbennett9@gmail.com>
2022-09-24 16:58:53 +0900

    deleting commented out tests

--- db.js
+++ db.js
@@ -24,36 +24,5 @@ const shortenUrl = (url, done) => {
   })
 }
 
-// shortenUrl('https://omg.com', data => console.log(data));
-
-// for first time tests of auto-increment
-// let testUrl1 = new Url({original_url: 'https://freecodecamp.com'});
-
-// let testUrl2 = new Url({original_url: 'https://coffeeguymike.com'});
-
-// let testUrl3 = new Url({original_url: 'https://google.com'});
-
-// testUrl1.save();
-// testUrl2.save();
-// testUrl3.save();
-
-// const testSchema = new Schema({
-//   name: {type: String, required: true},
-//   number: {type: Number}
-// });
-
-// testSchema.plugin(AutoIncrement, {id: 'order-seq', inc_field: 'number'})
-
-// const Test = mongoose.model('Test', testSchema);
-
-// let test = new Test({name: 'f'});
-
-// test.save();
-// console.log(test.number);
-
-// let test2 = new Test({name: 'h'});
-
-// test2.save();
-// console.log(test2.number);
 exports.shortenUrl = shortenUrl;
 exports.UrlModel = Url;
